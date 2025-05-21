890ba09b44a8d5369ae61310bbb7cadcc2f63744
tank-igorevich <alexandrstriii@gmail.com>
2025-05-21 14:35:27 +0300

    Add main and category_list views

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,12 @@
+from django.shortcuts import render
+from django.db.models import Count
+from .models import Recipe, Category
+
+def main(request):
+    recipes = Recipe.objects.order_by('-created_at')[:5]
+    return render(request, 'main.html', {'recipes': recipes})
+
+def category_list(request):
+    categories = Category.objects.annotate(recipe_count=Count('categories'))
+    return render(request, 'category_list.html', {'categories': categories})
 
